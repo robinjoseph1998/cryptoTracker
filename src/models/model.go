3be0102cd90d547b6b8ce53f598c14d7598a4afd
Robin Joseph <robinjoseph11998@gmail.com>
2024-07-17 01:25:17 +0530

    schema updated

--- src/models/model.go
+++ src/models/model.go
@@ -5,7 +5,7 @@ type Cryptocurrency struct {
 	ID               uint    `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name             string  `json:"name" gorm:"type:varchar(100);not null"`
 	Symbol           string  `json:"symbol" gorm:"type:varchar(100);not null"`
-	CurrentPrice     float64 `json:"current_price" gorm:"type:decimal(20,10);not null"`
+	CurrentPrice     float64 `json:"current_price" gorm:"type:decimal(30,10);not null"`
 	MarketCap        float64 `json:"market_cap" gorm:"type:decimal(20,2);not null"`
 	Volume24h        float64 `json:"volume_24h" gorm:"type:decimal(20,2);not null"`
 	PercentChange1h  float64 `json:"percent_change_1h" gorm:"type:decimal(5,2);not null"`
